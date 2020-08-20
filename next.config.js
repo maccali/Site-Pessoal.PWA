6d65ccf36aa50a7b31a4c4c291170deef8e5dae1
maccali <guilherme.maccali@universo.univates.br>
2020-08-20 01:27:21 -0300

    Teste

--- next.config.js
+++ next.config.js
@@ -4,15 +4,11 @@ const Dotenv = require("dotenv-webpack");
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    importScripts: [
-      '/worker.js'
-    ]
+    importScripts: ['/worker.js'],
   },
   webpack: (config) => {
-
     // Add the new plugin to the existing webpack plugins
     config.plugins.push(new Dotenv({ silent: true }));
-
     return config;
   },
   env: {
