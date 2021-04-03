f8fb9cd33bf595c480e058aa2ab4e5305901933f
maccali <guilherme.maccali@universo.univates.br>
2021-04-03 20:49:19 -0300

    fix: cache ajusts

--- next.config.js
+++ next.config.js
@@ -3,11 +3,29 @@
 
 const withPWA = require('next-pwa')
 const Dotenv = require('dotenv-webpack')
+const runtimeCaching = require('next-pwa/cache')
+const util = require('util')
+
+const cacheTime = 365 * 24 * 60 * 60 // 365 days
+
+runtimeCaching.map(item => {
+  item.options.expiration.maxAgeSeconds = cacheTime
+  if (item.options.cacheName === 'others') {
+    item.options.expiration.maxEntries = 200
+  }
+  if (item.options.cacheName === 'static-image-assets') {
+    item.options.expiration.maxEntries = 100
+  }
+})
+
+console.log('😜 RUNTIME CACHE =>', JSON.stringify(runtimeCaching, null, 4))
+// console.log('😜 RUNTIME CACHE =>', runtimeCaching)
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    importScripts: ['/worker.js']
+    importScripts: ['/worker.js'],
+    runtimeCaching
   },
   webpack: config => {
     // Add the new plugin to the existing webpack plugins
@@ -19,6 +37,6 @@ module.exports = withPWA({
     API_URL: process.env.API_URL
   },
   images: {
-    deviceSizes: [320, 640, 768, 1024, 1600],
+    deviceSizes: [320, 640, 768, 1024, 1600]
   }
 })
